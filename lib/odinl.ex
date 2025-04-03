a8bd5c3ab3eb82b6800d047307367de9ff7399bf
qq18562737 <18562737@qq.com>
2025-04-03 10:14:14 +0800

    11

---
+++ lib/odinl.ex
@@ -0,0 +1,18 @@
+defmodule Odinl do
+  @moduledoc """
+  Documentation for `Odinl`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Odinl.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/odinl_test.exs
@@ -0,0 +1,8 @@
+defmodule OdinlTest do
+  use ExUnit.Case
+  doctest Odinl
+
+  test "greets the world" do
+    assert Odinl.hello() == :world
+  end
+end
